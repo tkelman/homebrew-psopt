6fd6e2656a640f23d79a69f2dc88ecd49db20f25
vbox user <vboxuser@vboxs-Mac-mini.local>
2014-02-06 20:24:42 -0800

    initial commit

---
+++ lusol.rb
@@ -0,0 +1,82 @@
+require "formula"
+
+class Lusol < Formula
+  homepage 'http://www.stanford.edu/group/SOL/software/lusol.html'
+  url 'http://sourceforge.net/projects/lpsolve/files/LUSOL/2.2.1.0/LUSOL2.2.1.0.zip/download'
+  sha1 '9e12a0c774479a66146cf1de6936732d21c77087'
+
+  def patches
+    # fixes undefined symbols for max and _strupr
+    DATA
+  end
+
+  def install
+    # do case-insensitive comparison of file extensions
+    inreplace 'lusolmain.c', 'strcmp(fileext', 'strcasecmp(fileext'
+
+    system '$CC -O3 -c lusol.c mmio.c commonlib.c lusolio.c hbio.c myblas.c'
+    system 'ar cr liblusol.a lusol.o mmio.o commonlib.o lusolio.o hbio.o myblas.o'
+    system 'ranlib liblusol.a'
+    system '$CC -O3 lusolmain.c -L. -llusol -o lusol'
+
+    lib.install 'liblusol.a'
+    bin.install 'lusol'
+    include.install 'commonlib.h', 'myblas.h'
+    include.install Dir['lusol*.h']
+  end
+
+  test do
+    Afile = <<-EOS.undent
+      1 1 5.0
+      1 2 5.0
+      1 4 1.0
+      2 1 1.0
+      2 2 1.0
+      2 4 1.0
+    EOS
+    (testpath/'Afile.txt').write(Afile)
+    bfile = <<-EOS.undent
+      15.0
+      7.0
+    EOS
+    (testpath/'bfile.txt').write(bfile)
+    system 'lusol Afile.txt bfile.txt'
+  end
+end
+
+__END__
+diff --git a/lusolio.c b/lusolio.c
+index eb680bb..ae96073 100644
+--- a/lusolio.c
++++ b/lusolio.c
+@@ -43,8 +43,8 @@ MYBOOL ctf_read_A(char *filename, int maxm, int maxn, int maxnz,
+       jA[k]  = j;
+       Aij[k] = Ak;
+     }
+-    *m      = max( *m, i );
+-    *n      = max( *n, j );
++    if (i > *m) *m = i;
++    if (j > *n) *n = j;
+   }
+   fclose( iofile );
+   if(!eof) {
+diff --git a/lusolmain.c b/lusolmain.c
+index 224eaa0..4c1f10d 100644
+--- a/lusolmain.c
++++ b/lusolmain.c
+@@ -157,7 +157,6 @@ void main( int argc, char *argv[], char *envp[] )
+ 
+ /* Obtain file extension and see if we must estimate matrix data size */
+   strcpy(fileext, strchr(argv[useropt], '.'));
+-  _strupr(fileext);
+ 
+   /* Read conventional text file format */
+   if(strcmp(fileext, ".TXT") == 0) {
+@@ -257,7 +256,6 @@ void main( int argc, char *argv[], char *envp[] )
+     if(i != 0)
+       useropt++;
+     strcpy(fileext, strchr(argv[useropt], '.'));
+-    _strupr(fileext);
+ 
+     /* Read conventional text file format */
+     if(strcmp(fileext, ".TXT") == 0) {
---
+++ psopt.rb
@@ -0,0 +1,138 @@
+require 'formula'
+
+class Psopt302Patch < Formula
+  homepage 'https://code.google.com/p/psopt/'
+  url 'http://psopt.googlecode.com/files/patch_3.02.zip'
+  sha1 'fd04038126dcfe4e1e9d95a26c7795423ac73276'
+end
+
+class Psopt < Formula
+  homepage 'https://code.google.com/p/psopt/'
+  url 'http://psopt.googlecode.com/files/Psopt3.tgz'
+  sha1 '3cdcb7eb82bb3862376488026bd47413ec31161c'
+
+  #fails_with :clang do
+  #  build 500
+  #  cause <<-EOS.undent
+  #    The colon() DMatrix operator gives runtime "Dimension error in
+  #    elemDivision()" and elemProduct() functions when compiled with clang.
+  #    EOS
+  #end
+
+  def patches
+    # include assert and resolve ambiguous reference to rank in psopt.cxx,
+    # remove extern "C" around standard includes in dmatrixv.cxx
+    DATA
+  end
+
+  depends_on 'gnuplot' => :optional
+  depends_on 'openblas' => :optional
+  depends_on 'ipopt' => (build.with? 'openblas') ? ['with-openblas'] : :build
+  depends_on 'suite-sparse' => (build.with? 'openblas') ? ['with-openblas'] : :build
+  depends_on 'adol-c' => :build
+  depends_on 'lusol' => :build
+
+  def install
+    # Download and apply Psopt 3.02 patch
+    Psopt302Patch.new.brew do
+      (buildpath).install 'psopt.cxx'
+    end
+    # Recreate original copy of psopt.cxx for merging patches
+    system 'patch -t -o psopt.orig.cxx -d PSOPT -p2 -i ../000-homebrew.diff || true'
+    system 'diff -u PSOPT/psopt.orig.cxx psopt.cxx | patch -d PSOPT/src'
+
+    # Don't need to build CXSparse or LUSOL here
+    inreplace 'Makefile',
+              'all: $(CXSPARSE_LIBS) $(DMATRIX_LIBS) $(LUSOL_LIBS)',
+              'all: $(DMATRIX_LIBS)'
+
+    # Correct paths to dependencies
+    ipopt_prefix = Formula.factory('ipopt').prefix
+    adolc_prefix = Formula.factory('adol-c').prefix
+    suite_sparse_prefix = Formula.factory('suite-sparse').prefix
+    lusol_prefix = Formula.factory('lusol').prefix
+
+    inreplace ['PSOPT/lib/Makefile', 'PSOPT/examples/Makefile_linux.inc'] do |s|
+      s.change_make_var! 'prefix', ipopt_prefix
+    end
+
+    inreplace ['dmatrix/examples/Makefile', 'PSOPT/examples/Makefile_linux.inc'] do |s|
+      s.change_make_var! 'CXSPARSE', suite_sparse_prefix
+      s.change_make_var! 'LUSOL', lusol_prefix
+      s.change_make_var! 'SPARSE_LIBS', '$(LUSOL)/lib/liblusol.a $(CXSPARSE)/lib/libcxsparse.a'
+      if build.with? 'openblas'
+        s.change_make_var! 'FLIBS', "-L#{Formula.factory('openblas').lib} -lopenblas"
+      else
+        s.change_make_var! 'FLIBS', '-llapack -lblas'
+      end
+      # Remove unnecessary linker flags
+      s.gsub! '-lgcc_s', ''
+      s.remove_make_var! 'LDFLAGS'
+    end
+
+    inreplace 'PSOPT/examples/Makefile_linux.inc' do |s|
+      s.change_make_var! 'IPOPT_LIBS', "`cat #{ipopt_prefix}/share/coin/doc/Ipopt/ipopt_addlibs_cpp.txt`"
+      s.change_make_var! 'ADOLC_LIBS', "-L#{adolc_prefix}/lib -ladolc"
+    end
+
+    inreplace ['dmatrix/lib/Makefile',
+               'dmatrix/examples/Makefile',
+               'PSOPT/lib/Makefile',
+               'PSOPT/examples/Makefile_linux.inc'] do |s|
+      s.remove_make_var! 'CXX'
+    end
+
+    system 'make', 'all'
+
+    lib.install 'dmatrix/lib/libdmatrix.a'
+    lib.install 'PSOPT/lib/libpsopt.a'
+    include.install 'dmatrix/include/dmatrixv.h'
+    include.install Dir['PSOPT/src/*.h']
+    bin.install 'PSOPT/examples/obstacle/obstacle'
+    bin.install 'PSOPT/examples/bioreactor/bioreactor'
+    bin.install 'PSOPT/examples/brymr/brymr'
+  end
+end
+
+__END__
+diff --git a/PSOPT/src/psopt.cxx b/PSOPT/src/psopt.cxx
+index 18dc314..c9fd80d 100644
+--- a/PSOPT/src/psopt.cxx
++++ b/PSOPT/src/psopt.cxx
+@@ -92,6 +92,7 @@ _CRTIMP  int * __cdecl errno(void) { static int i=0; return &i; };
+ #include <math.h>
+ #include <string.h>
+ #include <time.h>
++#include <assert.h>
+ 
+ 
+ 
+@@ -9883,7 +9884,7 @@ void print_solution_summary(Prob& problem, Alg& algorithm, Sol& solution)
+         }
+       }
+ 
+-      fprintf(outfile,"\n\n>>>>> Rank of parameter covariance matrix: %i ", rank(Cp));
++      fprintf(outfile,"\n\n>>>>> Rank of parameter covariance matrix: %i ", ::rank(Cp));
+ 
+       fprintf(outfile,"\n\n>>> 95 percent statistical confidence limits on estimated parameters ");
+       fprintf(outfile,"\nPhase\tParameter\t(Low Confidence Limit) \t(Value) \t\t(High Confidence Limit)");
+diff --git a/dmatrix/src/dmatrixv.cxx b/dmatrix/src/dmatrixv.cxx
+index cc7e169..3579221 100755
+--- a/dmatrix/src/dmatrixv.cxx
++++ b/dmatrix/src/dmatrixv.cxx
+@@ -30,7 +30,6 @@ Author:    Dr. Victor M. Becerra
+ #ifdef UNIX
+ 
+ 
+-extern "C" {
+ 
+ 
+ #include <stdio.h>
+@@ -39,7 +38,6 @@ extern "C" {
+ #include <time.h>
+ #include <cstdio>
+ 
+-}
+ 
+ #else
+ 
